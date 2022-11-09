9dba76997a1a3a40ebd599ab08f1c62cec732ead
mazijason <chukwukachisom007@gmail.com>
2022-11-09 01:38:17 -0800

    github

--- builtins_help_1.c
+++ builtins_help_1.c
@@ -14,14 +14,17 @@ void help_all(void)
 	char *msg = "alias: alias [NAME[='VALUE'] ...]\n\tHandles aliases.\n";
 
 	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "\n\talias: Prints a list of all aliases, one per line, in ";		write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n\talias: Prints a list of all aliases, one per line, in ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 	msg = "the format NAME='VALUE'.\n\talias name [name2 ...]:prints";
 	write(STDOUT_FILENO, msg, _strlen(msg));
 	msg = " the aliases name, name2, etc. one per line, in the ";
 	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "form NAME='VALUE'.\n\talias NAME='VALUE' [...]: Defines";		write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "form NAME='VALUE'.\n\talias NAME='VALUE' [...]: Defines";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 	msg = " an alias for each NAME whose VALUE is given. If NAME ";
-	write(STDOUT_FILENO, msg, _strlen(msg));					msg = "is already an alias, replace its value with VALUE.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));			
+	msg = "is already an alias, replace its value with VALUE.\n";
 	write(STDOUT_FILENO, msg, _strlen(msg));
 }
 /**
