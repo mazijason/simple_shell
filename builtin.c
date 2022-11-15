b6b08c16516627fa92d23940a9857dabb35b8eea
Abdulrasheed Okhuosami <110449913+Qaadaffi@users.noreply.github.com>
2022-11-15 07:01:38 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -7,7 +7,7 @@ int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
 /**
  * get_builtin - Matches a command with a corresponding shellby builtin function.
- * @command: The command to match.
+ * @command: The command to match with builtin function.
  *
  * Return: A function pointer to the corresponding builtin.
  */
